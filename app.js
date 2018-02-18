75a5c96f55289efe3df8e73ed6eaaf5a15f2413d
march-dave <ysangy@gmail.com>
2018-02-18 18:11:15 -0500

    up to date

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var PORT = process.env.PORT || 3000;
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParse = require('body-parser');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParse.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+var server = http.createServer(app);
+
+server.listen(PORT, err => {
+    console.log(err || `Server listening on port ${PORT}`)
+});
