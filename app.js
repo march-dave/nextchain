a88ae64a64264fe6885e1a03d3d368d3ae0ba333
march-dave <ysangy@gmail.com>
2018-05-13 11:21:16 -0400

    some error fix

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ var MessageType = {
     RESPONSE_BLOCKCHAIN: 2
 };
 
-var blockchain = [getGenesisBlock()];
+// var blockchain = [getGenesisBlock()];
 
 var getGenesisBlock = () => {
     return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
-var blockchain = [getGenesisBlock()];
+// var blockchain = [getGenesisBlock()];
 
 var initHttpServer = () => {
     var app = express();
